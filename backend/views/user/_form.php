5b9b99021bd730f6bc0592c0a5c234b724b5f584
Ivan Konovskoy <konovskoy.i@gmail.com>
2020-11-28 05:02:16 +0700

    add russian text in views

--- backend/views/user/_form.php
+++ backend/views/user/_form.php
@@ -4,20 +4,28 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
-/* @var $model common\models\User */
+/* @var $model \frontend\models\SignupForm */
 /* @var $form yii\widgets\ActiveForm */
+/* @var $updateForm bool */
 ?>
 
 <div class="user-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+    <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
 
-    <?= $form->field($model, 'username')->textInput() ?>
+    <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
 
-    <?= $form->field($model, 'password_hash')->textInput()->label('Пароль') ?>
+    <?= $form->field($model, 'email') ?>
+
+    <?php if (!$updateForm): ?>
+
+    <?= $form->field($model, 'password')->passwordInput() ?>
+
+    <?php endif; ?>
+
 
     <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton('Сохранить', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
